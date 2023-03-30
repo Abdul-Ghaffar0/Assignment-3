8eba264df7702cb311e4d80cf455ccd04310454b
Abdul-Ghaffar0 <127423722+Abdul-Ghaffar0@users.noreply.github.com>
2023-03-31 03:28:48 +0500

    Add files via upload

---
+++ task4index.js
@@ -0,0 +1,9 @@
+// Declare a variable called isRaining and assign it a value of true
+let isRaining = true;
+
+// Check if it is raining using an if...else statement
+if (isRaining) {
+  console.log("Bring an umbrella");
+} else {
+  console.log("Leave the umbrella at home");
+}
